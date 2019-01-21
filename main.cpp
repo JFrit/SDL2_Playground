c211796fa59e9dd17dc7628ec57096d19386e00c
Jared Feeley <jf3925@rit.edu>
2019-01-20 21:19:36 -0500

    Added basic code to create a window and then close it after two seconds. Fixed the CMake build as well.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <cstdio>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[]) {
-    SDL_Window* window = NULL;
+    SDL_Window* window = nullptr;
 
-    SDL_Surface* screenSurface = NULL;
+    SDL_Surface* screenSurface = nullptr;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -15,8 +15,17 @@ int main(int argc, char* args[]) {
         window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                 SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-        if(window == NULL) {
+        if(window == nullptr) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
         }
     }
+    // Cleanup
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
